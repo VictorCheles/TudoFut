311ca486d55f4fe52d73d7c6da024f13f165b8f7
Victor Cheles <cheles@ESTACAODEV01.corregpm.correg.com>
2025-02-04 13:31:48 -0300

    ajuste retorno

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu Sistema</title>
+    <title>TudoFut</title>
+    <link rel="icon" href="{{ asset('images/logo_tudofut.svg') }}">
     @vite(['resources/js/app.js'])
 </head>
 <body>
